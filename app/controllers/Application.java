880b8d1cbf8aa3d5cc30fe4c0f7a37394dfe8846
Ruogu Hu <roc@broctany.com>
2013-01-19 12:25:14 -0500

    added checkin handlers

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,32 +4,80 @@ import models.Group;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+
+import play.libs.Json;
 
 public class Application extends Controller {
-	static Form<Group> groupForm = form(Group.class);
+    static Form<Group> groupForm = form(Group.class);
 
-	public static Result index() throws Exception {
-		// redirect to the "group Result
-		return redirect(routes.Application.group());
-	}
+    public static Result index() throws Exception {
+        // redirect to the "group Result
+        return redirect(routes.Application.group());
+    }
 
-	public static Result group() {
-		return ok(views.html.index.render(Group.all(), groupForm));
-	}
+    public static Result group() {
+        return ok(views.html.index.render(Group.all(), groupForm));
+    }
 
-	public static Result newGroup() {
-		Form<Group> filledForm = groupForm.bindFromRequest();
-		if(filledForm.hasErrors()) {
-			return badRequest(views.html.index.render(Group.all(), filledForm));
-		} else {
-			Group.create(filledForm.get());
-			return redirect(routes.Application.group());  
-		}
-	}
-	
-	public static Result deleteGroup(String id) {
-		Group.delete(id);
-		return redirect(routes.Application.group());
-	}
+    public static Result newGroup() {
+        Form<Group> filledForm = groupForm.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(views.html.index.render(Group.all(), filledForm));
+        } else {
+            Group.create(filledForm.get());
+            return redirect(routes.Application.group());  
+        }
+    }
+
+    public static Result deleteGroup(String id) {
+        Group.delete(id);
+        return redirect(routes.Application.group());
+    }
+
+    public static Result checkinFb(String fid) {
+        if (authenticated(fid)) {
+            return Application.checkin();
+        } else {
+            return Application.checkinFailed();
+        }
+    }
+
+    public static Result checkinFm(String usr, String pwd) {
+        if (authenticated(usr, pwd)) {
+            return Application.checkin();
+        } else {
+            return Application.checkinFailed();
+        }
+    }
+
+    private static Result checkin() {
+        JsonNode json = request().body().asJson();
+        if (json == null) {
+            return Application.checkinFailed();
+        } else {
+            String name = json.findPath("fid").getTextValue();
+            String truckid = json.findPath("truckid").getTextValue();
+            ObjectNode ok = Json.newObject();
+            ok.put("name", name);
+            ok.put("truckid", truckid);
+            return ok(ok);
+        }
+    }
+
+    public static Result checkinFailed() {
+        ObjectNode failed = Json.newObject();
+        failed.put("error", "authentication failed");
+        return badRequest(failed);
+    }
+    
+    private static boolean authenticated(String fid) {
+        return true;
+    }
+    
+    private static boolean authenticated(String usr, String pwd) {
+        return true;
+    }
 
 }
