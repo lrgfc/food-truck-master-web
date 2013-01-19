281458f1f74c15d984869e3bb99482388e4199d9
Chauncey <chenxi.wong@gmail.com>
2013-01-19 13:35:58 -0500

    Get Truck tested

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,37 @@
 package controllers;
 
+import java.util.List;
+
+import org.codehaus.jackson.node.ObjectNode;
+
+import com.google.gson.Gson;
+
 import models.Group;
 import play.data.Form;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
 	static Form<Group> groupForm = form(Group.class);
+
+//	 @BodyParser.Of(Json.class)
+	public static Result getTrucks() {		 
+		 Gson gson = new Gson();
+		 List<Group> groups = MorphiaObject.datastore.createQuery(Group.class).retrievedFields(false, "groupName").asList();
+		 String json = gson.toJson(groups);
+		 
+		 return ok(json);
+	}
+
+	// "name": String, 
+ //        "location": [Number, Number],
+ //        "address": String,
+ //        "phone": String,
+ //        "genre": String,
+ //        “photo”: String,
+ //        “average”: Number,
+ //        “count”: Number,
 
 	public static Result index() throws Exception {
 		// redirect to the "group Result
